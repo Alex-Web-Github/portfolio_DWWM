49b03265f552f14b1699efbceff8f548618446e8
Alexandre Foulc <alexandre-foulc@orange.fr>
2023-10-17 14:51:59 +0200

    Second Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mon Portofolio de Développeur Web
+
+## Quelques réalisations personnelles
+
+![cover](https://github.com/Alex-Web-Github/portofolio_DWWM/blob/main/screenshot.png)
+
+Consulter sur : <https://alexcreationweb.fr/>
---
+++ index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+
+  <title>AlexCréationWeb - Développeur Web</title>
+  <link rel="stylesheet" href="/assets/css/reset.css">
+  <link rel="stylesheet" href="/assets/css/styles.css">
+  <!-- <link rel="stylesheet" href="/assets/css/print.css" media="print"> -->
+
+  <meta name="description" content="Création de sites / application web entre Besançon et Pontarlier dans le Doubs et le Jura - Développeur WordPress">
+  <meta property="og:title" content="AlexCréationWeb - Développeur Web">
+  <meta property="og:description" content="Création de sites et application web">
+  <meta property="og:image" content="/assets/img/image_og_300-157.png">
+  <meta property="og:image:alt" content="">
+  <meta property="og:locale" content="fr_FR">
+  <meta property="og:type" content="website">
+  <meta property="og:site_name" content="alexcreationweb">
+  <meta name="twitter:card" content="summary">
+  <meta property="og:url" content="https://alexcreationweb.fr">
+  <link rel="canonical" href="https://alexcreationweb.fr/">
+
+  <link rel="icon" href="/favicon.ico" sizes="32x32">
+  <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+  <!-- ICON APPLE EN 192x192 -->
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+  <!-- For Android devices -->
+  <link rel="manifest" href="/manifest.webmanifest">
+  <meta name="theme-color" content="#000" media="(prefers-color-scheme: dark)">
+  <meta name="theme-color" content="#FFF" media="(prefers-color-scheme: light)">
+</head>
+
+<body>
+  <header class="header-l">
+    <div class="logo">
+      <img src="/assets/img/IMG_alex-mini.jpeg" alt="portrait alex" class="logo__img">
+      <p class="logo__title">Alexandre Foulc</p>
+      <p class="logo__subtitle">Développeur Web / Mobile<span class="underscore flash">_</span></p>
+    </div>
+    <nav class="navigation">
+      <a class="navigation__link active" href="#about">Présentation</a>
+      <a class="navigation__link" href="#skills">Compétences</a>
+      <a class="navigation__link" href="#portofolio">Réalisations</a>
+      <a class="navigation__link" href="#path">Parcours</a>
+    </nav>
+  </header>
+
+  <main class="main-l">
+    <div class="section__content section__info">
+      <a href="mailto:alexandre-foulc@orange.fr?Subject=Contact depuis AlexCréationWeb.fr" target="_blank" class="info__btn">M'envoyer un e-mail</a>
+      <a href="/assets/img/CV-FoulcAlex-v3.pdf" target="_blank" class="info__btn">Voir mon
+        CV</a>
+    </div>
+
+    <section class="section-l" id="about">
+      <h2 class="section__title">Présentation</h2>
+      <div class="section__content section__presentation">
+        <p class="presentation__text">
+          J'ai d'abord découvert l'Informatique dans les années 80 avec un Thomson TO7 avant de créer mon premier site Web au début des années 2000 avec le célèbre logiciel DreamWeaver. </br>Plus tard, avec l'arrivée de WordPress et la nécessité d'être présent sur Internet pour développer mon activité de Moniteur sportif, je me suis de nouveau intéressé aux nouvelles technologies du Web.</br>J'ai alors ressenti un besoin de me former pour mieux comprendre et ainsi acquérir de nouvelles compétences.
+        </p>
+        <p class="presentation__text">Curieux et rigoureux, je me suis découvert un réel intérêt pour la programmation et le Webdesign. Je continue à évoluer au gré des challenges.</p>
+        <p class="presentation__text">Aujourd'hui, je mets le même enthousiasme à réaliser un nouveau projet informatique qu'à guider mes clients au
+          travers d'<a href="//sportsnatureevasion.com" target="_blank">activités de pleine
+            nature.</a>
+        </p>
+      </div>
+    </section>
+    <hr>
+    <section class="section-l" id="skills">
+      <h2 class="section__title">Compétences</h2>
+      <div class="section__content section__skills">
+        <div class="skill__block">
+          <div class="block__title">
+            <img src="/assets/img/ui-design.png" class="skill__icon" alt="icon Front-End">
+            <h3 class="skill__title">Front-End</h3>
+          </div>
+
+          <ul class="skill__list">
+            <li class="skill__list__item">Maquetter une application</li>
+            <li class="skill__list__item">Réaliser un site statique et Responsive</li>
+            <li class="skill__list__item">Réaliser un site dynamique en JavaScript</li>
+            <li class="skill__list__item">Réaliser une interface utilisateur à l'aide d'un CMS</li>
+          </ul>
+        </div>
+        <div class="skill__block">
+          <div class="block__title">
+            <img src="/assets/img/back-end.png" class="skill__icon" alt="icon Back-End">
+            <h3 class="skill__title">Back-End</h3>
+          </div>
+
+          <ul class="skill__list">
+            <li class="skill__list__item">Concevoir une base de données</li>
+            <li class="skill__list__item">Concevoir des composants d'accès aux données</li>
+            <li class="skill__list__item">Développer la partie back-end d'une application web</li>
+            <li class="skill__list__item">Développer une application de gestion de contenu</li>
+          </ul>
+        </div>
+      </div>
+      <!-- Icones des Compétences -->
+      <div class="section__content__icons">
+        <img src="/assets/img/icons/html5-original.svg" class="icons" alt="icon html">
+        <img src="/assets/img/icons/css3-original.svg" class="icons" alt="icon css">
+        <img src="/assets/img/icons/javascript-original.svg" class="icons" alt="icon javascript">
+        <img src="/assets/img/icons/bootstrap-original.svg" class="icons" alt="icon bootstrap">
+        <img src="/assets/img/icons/php-original.svg" class="icons" alt="icon php">
+        <img src="/assets/img/icons/mysql-original-wordmark.svg" class="icons" alt="icon mysql">
+        <img src="/assets/img/icons/symfony-original-wordmark.svg" class="icons" alt="icon symfony">
+        <img src="/assets/img/icons/wordpress-original.svg" class="icons" alt="icon wordpress">
+        <img src="/assets/img/icons/figma-original.svg" class="icons" alt="icon figma">
+        <img src="/assets/img/icons/vscode-original-wordmark.svg" class="icons" alt="icon vscode">
+        <img src="/assets/img/icons/git-original-wordmark.svg" class="icons" alt="icon git">
+        <img src="/assets/img/icons/github-original.svg" class="icons" alt="icon github">
+
+
+      </div>
+      <!-- Icones des Compétences -->
+    </section>
+    <hr>
+    <section class="section-l" id="portofolio">
+      <h2 class="section__title">Réalisations</h2>
+      <div class="section__content section__project">
+        <div class="project">
+          <div class="project__cover" style="background-image: url(/assets/img/thumbnail-CDS25-mini.jpeg); "></div>
+          <div class="project__content">
+            <span class="project__badge">WordPress</span>
+            <p class="project__title">C.D.S. 25</p>
+            <p class="project__description">Le site web du Comité Départemental de Spéléologie du Doubs</p>
+            <div class="project__footer"><a href="https://www.speleo-doubs.com/" target="_blank">Voir le projet</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="project">
+          <div class="project__cover" style="background-image: url(/assets/img/thumbnail-dgsoudure-mini-1.jpeg); ">
+          </div>
+          <div class="project__content">
+            <span class="project__badge">WordPress</span>
+            <p class="project__title">DG Soudure Création</p>
+            <p class="project__description">Le site web d'un artisan métallier-soudeur près Besançon</p>
+            <div class="project__footer"><a href="https://dgsoudure.fr" target="_blank">Voir le projet</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="project">
+          <div class="project__cover" style="background-image: url(/assets/img/thumbnail-quaiantique-mini.jpeg); ">
+          </div>
+          <div class="project__content">
+            <span class="project__badge">PHP-MySQL</span>
+            <p class="project__title">Quai Antique</p>
+            <p class="project__description">Le site web d'un restaurant avec Back-Office et Réservation en ligne </p>
+            <div class="project__footer"><a href="http://sc4foal9574.universe.wf/quai_antique/" target="_blank">Voir le
+                projet</a>
+            </div>
+          </div>
+        </div>
+        <div class="project">
+          <div class="project__cover" style="background-image: url(/assets/img/thumbnail-charles-cantin-mini.jpeg); ">
+          </div>
+          <div class="project__content">
+            <span class="project__badge">WordPress</span>
+            <p class="project__title">Charles Cantin</p>
+            <p class="project__description">Le site web d'un photographe Freelance / Thème WP sur-mesure</p>
+            <div class="project__footer"><a href="http://sc4foal9574.universe.wf/charles-cantin/" target="_blank">Voir
+                le projet</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="project">
+          <div class="project__cover" style="background-image: url(/assets/img/thumbnail-rolldice-mini.jpeg); ">
+          </div>
+          <div class="project__content">
+            <span class="project__badge">HTML / CSS / JS</span>
+            <p class="project__title">Roll Dice</p>
+            <p class="project__description">Un jeu de dé pour jouer dans votre navigateur</p>
+            <div class="project__footer"><a href="https://alex-web-github.github.io/Jeu-RollDice-JS/" target="_blank">Voir
+                le projet</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="project">
+          <div class="project__cover" style="background-image: url(/assets/img/thumbnail-sportsnatureevasion-mini.jpeg); ">
+          </div>
+          <div class="project__content">
+            <span class="project__badge">WordPress</span>
+            <p class="project__title">SportsNatureÉvasion</p>
+            <p class="project__description">Le site web de mon activité de Guide / Éducateur sportif</p>
+            <div class="project__footer"><a href="https://www.sportsnatureevasion.com" target="_blank">Voir
+                le projet</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="project">
+          <div class="project__cover" style="background-image: url(/assets/img/thumbnail-clipboard-mini.jpeg); ">
+          </div>
+          <div class="project__content">
+            <span class="project__badge">Webdesign</span>
+            <p class="project__title">ClipBoard</p>
+            <p class="project__description">Maquette sous Figma d'une application web fictive (desktop/mobile)</p>
+            <div class="project__footer"><a href="/assets/img/Clipboard-Design_compressed.pdf" target="_blank">Voir
+                le projet</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <hr>
+    <section class="section-l" id="path">
+      <h2 class="section__title">Parcours</h2>
+      <div class="section__content section__path">
+        <div class="step step--left">
+          <p class="step__title">STUDI / Digital School - 2023</p>
+          <p class="step__description">Dîplomé d'un titre professionnel de Développeur Web / Web Mobile</p>
+        </div>
+        <div class="step step--right">
+          <p class="step__title">WP Chef - 2021</p>
+          <p class="step__description">Certification RS 5170 : Concevoir des sites WordPress professionnels</p>
+        </div>
+        <div class="step step--left">
+          <p class="step__title">École Française de Papeterie et des Industries
+            Graphiques de Grenoble</p>
+          <p class="step__description">Obtention du diplôme d’Ingénieur en 09/2000</p>
+        </div>
+        <div class="step step--right">
+          <p class="step__title">Classes Préparatoires (Physique-Technologie) 1994 - 1997</p>
+          <p class="step__description">Lycée Vaucanson - Grenoble</p>
+        </div>
+        <div class="step step--left">
+          <p class="step__title">Baccalauréat C (Mathématiques - Physique) - 1994</p>
+          <p class="step__description">Lycée Champollion - Grenoble</p>
+        </div>
+      </div>
+    </section>
+
+
+  </main>
+
+  <footer class="footer-l">
+    <p class="footer__text">AlexCréationWeb | 2022 -
+      <?= date("Y"); ?>
+    </p>
+  </footer>
+
+  <script src="/assets/js/global.js"></script>
+</body>
+
+</html>
