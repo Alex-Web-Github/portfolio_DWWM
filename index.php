efc464cc05d4e3d88ebdfed6f548da6dac408efd
Alexandre Foulc <alexandre-foulc@orange.fr>
2025-02-07 15:17:12 +0100

    Modif Design

--- index.php
+++ index.php
@@ -31,44 +31,59 @@
   <meta name="theme-color" content="#FFF" media="(prefers-color-scheme: light)">
 </head>
 
-<body class="global-container">
+<body>
   <header>
     <div class="header-l">
-      <div class="logo">
-        <img src="/img/IMG_alex-mini.jpeg" alt="portrait Alexandre Foulc" class="logo__img">
-        <p class="logo__title">Alexandre Foulc</p>
-        <p class="logo__subtitle">Développeur Web<span class="underscore flash">_</span></p>
-      </div>
-      <!-- <nav class="navigation">
-        <a class="navigation__link" href="#skills">Compétences</a>
+      <div class="logo-nav">
+        <div class="logo">
+          <div class="logo__img">
+            <img src="/img/image_og_300-157.webp" alt="Logo AlexCréationWeb">
+          </div>
+          <p class="logo__title">AlexCréationWeb<span class="underscore flash">_</span></p>
+        </div>
+        <!-- <nav class="navigation">
+        <a class="navigation__link active" href="#skills">Compétences</a>
         <a class="navigation__link" href="#projects">Réalisations</a>
         <a class="navigation__link" href="#path">Parcours</a>
         <a class="navigation__link" href="#about">A propos</a>
-      </nav> -->
+        </nav> -->
+      </div>
+      <div class="hero">
+        <div class="block__hero">
+          <div class="hero__img">
+            <img src="/img/IMG_alex-mini.jpeg" alt="Alexandre Foulc développeur web">
+          </div>
+          <div class="hero__content">
+            <h1 class="hero__title">Alexandre Foulc</h1>
+            <p class="hero__subtitle">Développeur PHP | WordPress</p>
+            <p class="hero__description">Passionné et créatif, j'aime développer des applications web évolutives et performantes.</p>
+          </div>
+        </div>
+        <div class="block__btn">
+          <a type="button" class="info__btn" href="https://www.linkedin.com/in/alexandre-foulc/" target="_blank" title="Profil LinkedIn">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+              <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
+            </svg>LinkedIn
+          </a>
+
+          <a type="button" class="info__btn" href="https://github.com/Alex-Web-Github" target="_blank" title="Compte Github">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+            </svg>Github
+          </a>
+
+          <a type="button" class="info__btn" href="mailto:contact@alexcreationweb.fr?Subject=Contact depuis AlexCréationWeb.fr" target="_blank" title="M'envoyer un mail">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+              <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
+            </svg>contact@alexcreationweb.fr
+          </a>
+
+          <a target="_blank" class="info__btn disabled" role="link" aria-disabled="true" title="CV disponible sur demande">Mon CV
+          </a>
+
+        </div>
+      </div>
     </div>
-    <section class="section__btn">
-      <a type="button" class="info__btn" href="https://www.linkedin.com/in/alexandre-foulc/" target="_blank" title="Profil LinkedIn">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-          <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
-        </svg>LinkedIn
-      </a>
-
-      <a type="button" class="info__btn" href="https://github.com/Alex-Web-Github" target="_blank" title="Compte Github">
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-          <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
-        </svg>Github
-      </a>
-
-      <a type="button" class="info__btn" href="mailto:contact@alexcreationweb.fr?Subject=Contact depuis AlexCréationWeb.fr" target="_blank" title="M'envoyer un mail">
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
-          <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
-        </svg>contact@alexcreationweb.fr
-      </a>
-
-      <a target="_blank" class="info__btn disabled" role="link" aria-disabled="true" title="CV disponible sur demande">Mon CV
-      </a>
-
-    </section>
   </header>
 
   <main class="main-l">
@@ -221,14 +236,11 @@
       <h2 class="section__title">Qui suis-je ?</h2>
       <div class="section__content">
         <p class="presentation__text">
-          J'ai d'abord découvert l'Informatique dans les années 80 avec un Thomson TO7 avant de créer un premier site Web au début des années 2000 avec le célèbre logiciel DreamWeaver. </br>Plus tard, avec l'arrivée de WordPress et la nécessité d'être présent sur Internet pour développer mon activité de Moniteur sportif, je me suis de nouveau intéressé aux nouvelles technologies du Web.</br>J'ai alors ressenti un besoin de me former pour mieux comprendre et ainsi acquérir de nouvelles compétences.
+          J'ai d'abord découvert l'Informatique dans les années 80 avec un Thomson TO7 avant de créer un premier site Web au début des années 2000 avec le célèbre logiciel DreamWeaver. </br>Plus tard, avec la nécessité d'être présent sur Internet pour développer <span class="text--underline">
+            <a href="https://sportsnatureevasion.com" target="_blank">mon activité de Moniteur sportif</a>
+          </span>, je me suis de nouveau intéressé aux nouvelles technologies du Web.</br>J'ai alors ressenti l'envie de me former pour mieux comprendre et ainsi acquérir de nouvelles compétences.
         </p>
         <p class="presentation__text">Curieux et rigoureux, je me suis découvert un réel intérêt pour la programmation et le webdesign. Aujourd'hui, je continue à évoluer au gré des challenges.</p>
-        <p class="presentation__text">Je mets le même enthousiasme à réaliser un nouveau projet informatique qu'à guider mes clients à
-          travers des<span class="text--underline">
-            <a href="https://sportsnatureevasion.com" target="_blank">activités de pleine nature.</a>
-          </span>
-        </p>
       </div>
     </section>
     <hr>
@@ -269,7 +281,7 @@
     </div>
   </footer>
 
-  <script src="/js/global.js"></script>
+  <!-- <script src="/js/global.js"></script> -->
 
 </body>
 
